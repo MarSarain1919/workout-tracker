51df879656f19d66880c6df828a96f3ee6a7d6ca
Katja Sarain <katjasarain@gmail.com>
2021-07-21 22:55:59 -0700

    final

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
 
+// get exercises
+
+router.get("/exercise", async (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+})
+
+// get stats
+
+router.get("/stats", async (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+})
+
+// get workouts
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then(workoutTrackerDb => {res.json(workoutTrackerDb);})
+        .catch(err => {res.status(400).json(err);});
+    });
+
+// post workouts
+
+router.post("/api/workouts", ({body}, res) => {
+    Workout.create(body)
+        .then(workoutTrackerDb => {res.json(workoutTrackerDb);})
+        .catch(err => {res.status(400).json(err);});
+    });
+
+// update workout
+
+// get workout range
 
 
 module.exports = router;
